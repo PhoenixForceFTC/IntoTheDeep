c2a02eb1d42c32c4d38c6e249e313c40586eea32
rishibellam <119266305+rishibellam@users.noreply.github.com>
2024-11-03 19:46:42 -0600

    summary

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/Lift.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.core.tools;
+
+import static org.firstinspires.ftc.teamcode.util.Utilities.listFromParams;
+
+import androidx.annotation.Nullable;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.util.MathUtils;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.core.Subsystem;
+import java.util.ArrayList;
+import java.util.function.DoubleSupplier;
+
+@Config
+public class Lift implements Subsystem {
+    public enum Position {
+        BOTTOM_POSITION_CONTROL(0),
+        TOP_POSITION_CONTROL(1800),
+        MANUAL(-1),
+        CUSTOM(-1);
+
+        public final int height;
+
+        Position(int ticks) {
+            this.height = ticks;
+        }
+    }
+
+    private final ArrayList<MotorEx> motors;
+    private final MotorEx left;
+    private final PIDFController feedbackControllerLeft;
+    private final DoubleSupplier manual;
+
+    public static Position target = Position.BOTTOM_POSITION_CONTROL;
+    public static int customHeight = 0;
+
+    public static double liftTickSpeed = 30;
+    private final Telemetry telemetry;
+
+    private int lastTargetTicks = 0;
+    public static double kP = 0.02, kI = 0.007, kD = 0.0005, kF = 0.002;
+    public Lift(HardwareMap hardwareMap, Telemetry telemetry, @Nullable DoubleSupplier manualPowerController) {
+        Lift.target = Position.BOTTOM_POSITION_CONTROL;
+
+        left = new MotorEx(hardwareMap, "liftA", Motor.GoBILDA.RPM_435);
+        left.stopAndResetEncoder();
+
+        motors = listFromParams(left);
+        feedbackControllerLeft = new PIDFController(kP, kI, kD, kF);
+
+        this.manual = manualPowerController;
+        this.telemetry = telemetry;
+    }
+
+    @Override
+    public void update() {
+        feedbackControllerLeft.setPIDF(kP, kI, kD, kF);
+
+        int targetTicks = target.height;
+
+        final int currentLeft = getCurrentLeftPosition();
+
+        switch (target) { // OVERRIDES
+            case MANUAL:
+                final double gamepad = manual.getAsDouble();
+                targetTicks = Math.max(
+                        Position.BOTTOM_POSITION_CONTROL.height,
+                        Math.min(
+                                Position.TOP_POSITION_CONTROL.height,
+                                lastTargetTicks + ((int) Math.round(gamepad * liftTickSpeed))
+                        )
+                );
+                lastTargetTicks = targetTicks;
+                break;
+            case CUSTOM:
+                targetTicks = customHeight;
+                break;
+        }
+
+        final double feedbackOutputLeft = currentLeft > 50 || targetTicks > 10 ? feedbackControllerLeft.calculate(currentLeft, targetTicks) : 0;
+        telemetry.addData("current left lift", currentLeft);
+        telemetry.addData("target lift", targetTicks);
+
+        left.set(MathUtils.clamp(feedbackOutputLeft, -1, 1));}
+
+    public int getCurrentLeftPosition() {
+        return left.getCurrentPosition();
+    }
+
+
+
+    public void setPower(double power) {
+        motors.forEach(m -> m.set(power));
+    }
+
+    public void setTargetPosition(Position position) {
+        lastTargetTicks = Lift.target.height;
+        Lift.target = position;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/Tooling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/Tooling.java
@@ -11,15 +11,13 @@ import org.firstinspires.ftc.teamcode.core.Subsystem;
 
 @Config
 public class Tooling implements Subsystem {
-    final Arm arm;
-    final MultipleMotorLift lift;
+    final Lift lift;
     final ToggleablePositionServo claw;
     final GamepadEx gamepad;
 
     public static double tickIncreasePerLoop = 40D;
     public Tooling(HardwareMap hardwareMap, Telemetry telemetry, GamepadEx toolGamepad) {
-        this.arm = new Arm(hardwareMap, telemetry, toolGamepad::getRightY);
-        this.lift = new MultipleMotorLift(hardwareMap, telemetry, toolGamepad::getLeftY);
+        this.lift = new Lift(hardwareMap, telemetry, toolGamepad::getLeftY);
         this.claw = new ToggleablePositionServo(hardwareMap, .8, .2, "claw", true);
         this.gamepad = toolGamepad;
 
@@ -30,43 +28,22 @@ public class Tooling implements Subsystem {
     public void update() {
         gamepad.readButtons();
 
-        if (gamepad.wasJustReleased(GamepadKeys.Button.RIGHT_STICK_BUTTON)) {
-            arm.setTargetPosition(Arm.Position.MANUAL);
-        } else if (gamepad.wasJustReleased(GamepadKeys.Button.X)) {
-            arm.setTargetPosition(Arm.Position.PENETRATION);
-            Arm.extensionPosition = 0;
-        } else if (gamepad.wasJustReleased(GamepadKeys.Button.A)) {
-            arm.setTargetPosition(Arm.Position.GRABBING_TELEOP);
-            Arm.extensionPosition = 0;
-        } else if (gamepad.wasJustReleased(GamepadKeys.Button.START)) {
-            arm.setTargetPosition(Arm.Position.HOME);
-            Arm.extensionPosition = 0;
-        }
+
         final double rightTrigger = gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
         final double leftTrigger = gamepad.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER);
-        Arm.extensionPosition = Math.max(0, Math.min(Arm.MAX_EXTENSION, Arm.extensionPosition + (int) Math.round((rightTrigger > leftTrigger ? rightTrigger : leftTrigger * -1) * tickIncreasePerLoop)));
-
-        if (gamepad.wasJustReleased(GamepadKeys.Button.LEFT_STICK_BUTTON)) {
-            lift.setTargetPosition(MultipleMotorLift.Position.MANUAL);
+        if (gamepad.wasJustReleased(GamepadKeys.Button.RIGHT_STICK_BUTTON)) {
+            lift.setTargetPosition(Lift.Position.MANUAL);
         } else if (gamepad.wasJustReleased(GamepadKeys.Button.DPAD_UP)) {
-            lift.setTargetPosition(MultipleMotorLift.Position.TOP_POSITION_CONTROL);
-            arm.setTargetPosition(Arm.Position.DUMPING);
-            Arm.extensionPosition = 500;
+            lift.setTargetPosition(Lift.Position.TOP_POSITION_CONTROL);
         } else if (gamepad.wasJustReleased(GamepadKeys.Button.DPAD_DOWN)) {
-            lift.setTargetPosition(MultipleMotorLift.Position.BOTTOM_POSITION_CONTROL);
-            arm.setTargetPosition(Arm.Position.PENETRATION);
-            Arm.extensionPosition = 0;
+            lift.setTargetPosition(Lift.Position.BOTTOM_POSITION_CONTROL);
         }
 
         if (gamepad.wasJustReleased(GamepadKeys.Button.RIGHT_BUMPER)) {
             claw.toggle();
         }
 
-        if (gamepad.wasJustReleased(GamepadKeys.Button.LEFT_BUMPER)) {
-            arm.resetArmPosition();
-        }
 
-        arm.update();
         lift.update();
         claw.update();
 
