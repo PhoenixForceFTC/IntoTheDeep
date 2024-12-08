cbef0ea9289d4f2a9ce287dda95e9d41c14f5d8f
Ethan Sanders <doawk7@gmail.com>
2024-12-08 15:26:15 -0600

    tooling

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/Claw.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.core.tools;
+
+public interface Claw {
+    void open();
+    void close();
+    boolean toggle();
+    boolean getState();
+    void setState();
+}
