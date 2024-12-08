4220418a14e2c86aac2513894d676576b1ce5748
Ethan Sanders <doawk7@gmail.com>
2024-12-08 15:27:55 -0600

    tooling

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/ClawImpl.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.core.tools;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ClawImpl implements Claw {
+    // PLACEHOLDER
+
+    public ClawImpl(HardwareMap hardwareMap) {
+
+    }
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean toggle() {
+        return false;
+    }
+
+    @Override
+    public boolean getState() {
+        return false;
+    }
+
+    @Override
+    public void setState() {
+
+    }
+}
