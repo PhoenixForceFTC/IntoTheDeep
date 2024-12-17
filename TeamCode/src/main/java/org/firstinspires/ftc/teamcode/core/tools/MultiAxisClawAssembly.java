e621a9ec2ef3228f2af83d1149e8101aede6a57d
jaiwalkkin <152460044+jaiwalkkin@users.noreply.github.com>
2024-12-16 20:19:20 -0600

    ughhhhh claw code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/MultiAxisClawAssembly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/tools/MultiAxisClawAssembly.java
@@ -1,14 +1,21 @@
 package org.firstinspires.ftc.teamcode.core.tools;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.util.MathUtils;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
 
+import dev.frozenmilk.util.units.angle.Angle;
+import dev.frozenmilk.util.units.angle.AngleUnits;
+import dev.frozenmilk.util.units.angle.Angles;
+
+@Config
 public class MultiAxisClawAssembly extends ToggleablePositionServo {
-    private static final double DEGREES_TO_SERVO_MULT = 0;
-    private static final double PITCH_SERVO_OFFSET = 0, ROLL_SERVO_OFFSET = 0;
+    private static double SERVO_TICKS_PER_DEGREE = 1 / 355D;
+    private static double PITCH_SERVO_OFFSET = 0, ROLL_SERVO_OFFSET = 0;
+    private final Servo pitch, roll;
 
-    final Servo pitch, roll;
+    private Angle pitchAngle = Angles.wrappedDeg(0), rollAngle = Angles.wrappedDeg(0);
     // add setpoint variables
     public MultiAxisClawAssembly(HardwareMap hardwareMap) {
         super(hardwareMap, .8, .2, "claw", false);
@@ -18,8 +25,39 @@ public class MultiAxisClawAssembly extends ToggleablePositionServo {
     @Override
     public void update() {
         super.update();
-        // update pitch/roll to setpoints
+        setServos();
     }
-    // set rotation values for each axis servo (apply offsets and multiplier)
-    // get rotation values of servo
+
+    private void setServos() {
+        pitch.setPosition(
+                MathUtils.clamp(
+                        pitchAngle.get(AngleUnits.DEGREE) * SERVO_TICKS_PER_DEGREE + PITCH_SERVO_OFFSET,
+                        0,
+                        1
+                )
+        );
+        roll.setPosition(
+                MathUtils.clamp(
+                        rollAngle.get(AngleUnits.DEGREE) * SERVO_TICKS_PER_DEGREE + ROLL_SERVO_OFFSET,
+                        0,
+                        1
+                )
+        );
+    }
+
+    public Angle getPitchAngle() {
+        return pitchAngle;
+    }
+
+    public void setPitchAngleDegrees(double degrees) {
+        this.pitchAngle = Angles.wrappedDeg(degrees);
+    }
+
+    public Angle getRollAngle() {
+        return rollAngle;
+    }
+
+    public void setRollAngleDegrees(double degrees) {
+        this.rollAngle = Angles.wrappedDeg(degrees);
+    }
 }
