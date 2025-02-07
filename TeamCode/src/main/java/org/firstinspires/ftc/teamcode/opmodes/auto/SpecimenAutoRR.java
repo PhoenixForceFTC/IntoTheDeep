dcd069b41bb4f21da1a5ae9e8f0e7552b54b4b4a
rishibellam <119266305+rishibellam@users.noreply.github.com>
2025-02-06 21:30:01 -0600

    code that we are going to push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAutoRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAutoRR.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.core.tools.Arm;
 import org.firstinspires.ftc.teamcode.core.tools.MultiAxisClawAssembly;
 import org.firstinspires.ftc.teamcode.core.tools.Tooling;
+import org.firstinspires.ftc.teamcode.opmodes.auto.AutoOpMode3deadwheel;
 
 @Config
 @Autonomous(group="!CompOpModes")
@@ -25,19 +26,21 @@ public class SpecimenAutoRR extends AutoOpMode3deadwheel {
     public static Position START = new Position(7.5,-67,90);
     public static Position START2 = new Position(7.5,-63,90);
     public static Position SCORE1 = new Position(0, -63,90);
-    public static Position SCORE2 = new Position(0, -29,90);
-    public static Position INT1 = new Position(24,-54,0);
-    public static Position INT2 = new Position(36,-24,270);
-    public static Position BLOCK1 = new Position(46,-12,270);
-    public static Position BLOCK1DEPOSIT = new Position(46,-60,270);
-    public static Position BLOCK2 = new Position(57,-10,270);
-    public static Position BLOCK2DEPOSIT = new Position(57,-60,270);
-    public static Position BLOCK3 = new Position(61,-9,270);
-    public static Position BLOCK3DEPOSIT = new Position(61 ,-60,270);
+    public static Position SCORE2 = new Position(0, -40,90);
+    public static Position SCORE3 = new Position(0, -48,90);
+    public static Position INT1 = new Position(34,-48,90);
+    public static Position INT2 = new Position(36,-12,90);
+    public static Position BLOCK1 = new Position(46,-12,90);
+    public static Position BLOCK1DEPOSIT = new Position(46,-60,90);
+    public static Position BLOCK2 = new Position(57,-10,90);
+    public static Position BLOCK2DEPOSIT = new Position(57,-60,90);
+    public static Position GRAB1 = new Position( 46,-36,270);
+    public static Position GRAB2 = new Position(36,-48,270);
+    public static Position SCORE4 = new Position(0, -34,270);
 
 
 
-    
+
     @Override
     public void runOpMode() {
 
@@ -51,28 +54,25 @@ public class SpecimenAutoRR extends AutoOpMode3deadwheel {
         goTo(SCORE1);
         Arm.customAngle = 0;
         Arm.extensionPosition = 0;
-        Arm.customAngle = Arm.Position.DUMPING.angle;
-        Arm.extensionPosition = 1010;
-        arm.update();
+        Arm.extensionPosition = 1300;
+        sleepTools(1000);
+        Arm.customAngle = 70;
+        sleepTools(1000);
         claw.setPosition(MultiAxisClawAssembly.Position.SUBMERSIBLE_PICKUP_HORIZONTAL);
         Arm.customAngle = 67;
-        arm.update();
+        sleepTools(1000);
         goTo(SCORE2);
         Arm.extensionPosition = 0;
-        sleepTools(2000);
-        //goTo(SCORE1);
+        sleepTools(1000);
         claw.off();
-        //goTo(INT1);
-        //goTo(INT2);
-        //goTo(BLOCK1);
-        Action traj = drive.actionBuilder(drive.localizer.getPose())
-
-                .splineTo(INT1.toVector2d(), INT1.toRotation2d(), new TranslationalVelConstraint(100)/*, accelConstraint*/)//new splines wont be implemented through autoOpMode
-                .splineTo(INT2.toVector2d(), INT2.toRotation2d(), new TranslationalVelConstraint(100)/*, accelConstraint*/)
-                .splineToConstantHeading(BLOCK1.toVector2d(), BLOCK1.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)
-                .build();
-
-        Actions.runBlocking(traj);
+        sleepTools(1000);
+        goTo(SCORE3);
+        claw.off();
+        goTo(INT1);
+        goTo(INT2);
+        claw.setPosition(MultiAxisClawAssembly.Position.WALL_SPECIMEN_PICKUP);
+        arm.update();
+        goTo(BLOCK1);
         goTo(BLOCK1DEPOSIT);
         Action traj2 = drive.actionBuilder(drive.localizer.getPose())
 
@@ -82,11 +82,18 @@ public class SpecimenAutoRR extends AutoOpMode3deadwheel {
 
         Actions.runBlocking(traj2);
         goTo(BLOCK2DEPOSIT);
-        goTo(BLOCK2);
-        setSpeed(Speed.SLOW);
-        goTo(BLOCK3);
-        setSpeed(Speed.FAST);
-        goTo(BLOCK3DEPOSIT);
+        Action traj3 = drive.actionBuilder(drive.localizer.getPose())
+
+                .splineTo(GRAB1.toVector2d(), GRAB1.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)//new splines wont be implemented through autoOpMode
+                .splineToConstantHeading(GRAB2.toVector2d(), GRAB2.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)
+                .build();
+
+        Actions.runBlocking(traj3);
+        arm.setTargetAngle(Arm.Position.HOME);
+        claw.setPosition(MultiAxisClawAssembly.Position.WALL_SPECIMEN_PICKUP);
+        goTo(SCORE4);
+
+
     }
 
 }
