19d3e93225ff69ddac0cce7012a0e9f88da162a2
rishibellam <119266305+rishibellam@users.noreply.github.com>
2025-02-06 19:40:57 -0600

    spline auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAutoRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAutoRR.java
@@ -25,15 +25,15 @@ public class SpecimenAutoRR extends AutoOpMode3deadwheel {
     public static Position START = new Position(7.5,-67,90);
     public static Position START2 = new Position(7.5,-63,90);
     public static Position SCORE1 = new Position(0, -63,90);
-    public static Position SCORE2 = new Position(0, -26,90);
-    public static Position INT1 = new Position(34,-60,90);
-    public static Position INT2 = new Position(36,-12,90);
-    public static Position BLOCK1 = new Position(46,-12,90);
-    public static Position BLOCK1DEPOSIT = new Position(46,-60,90);
-    public static Position BLOCK2 = new Position(57,-10,90);
-    public static Position BLOCK2DEPOSIT = new Position(57,-60,90);
-    public static Position BLOCK3 = new Position(61,-9,90);
-    public static Position BLOCK3DEPOSIT = new Position(61 ,-60,90);
+    public static Position SCORE2 = new Position(0, -29,90);
+    public static Position INT1 = new Position(24,-54,0);
+    public static Position INT2 = new Position(36,-24,270);
+    public static Position BLOCK1 = new Position(46,-12,270);
+    public static Position BLOCK1DEPOSIT = new Position(46,-60,270);
+    public static Position BLOCK2 = new Position(57,-10,270);
+    public static Position BLOCK2DEPOSIT = new Position(57,-60,270);
+    public static Position BLOCK3 = new Position(61,-9,270);
+    public static Position BLOCK3DEPOSIT = new Position(61 ,-60,270);
 
 
 
@@ -60,14 +60,27 @@ public class SpecimenAutoRR extends AutoOpMode3deadwheel {
         goTo(SCORE2);
         Arm.extensionPosition = 0;
         sleepTools(2000);
-        goTo(SCORE1);
+        //goTo(SCORE1);
         claw.off();
-        goTo(INT1);
-        goTo(INT2);
-        goTo(BLOCK1);
+        //goTo(INT1);
+        //goTo(INT2);
+        //goTo(BLOCK1);
+        Action traj = drive.actionBuilder(drive.localizer.getPose())
+
+                .splineTo(INT1.toVector2d(), INT1.toRotation2d(), new TranslationalVelConstraint(100)/*, accelConstraint*/)//new splines wont be implemented through autoOpMode
+                .splineTo(INT2.toVector2d(), INT2.toRotation2d(), new TranslationalVelConstraint(100)/*, accelConstraint*/)
+                .splineToConstantHeading(BLOCK1.toVector2d(), BLOCK1.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)
+                .build();
+
+        Actions.runBlocking(traj);
         goTo(BLOCK1DEPOSIT);
-        goTo(BLOCK1);
-        goTo(BLOCK2);
+        Action traj2 = drive.actionBuilder(drive.localizer.getPose())
+
+                .splineToConstantHeading(BLOCK1.toVector2d(), BLOCK1.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)//new splines wont be implemented through autoOpMode
+                .splineToConstantHeading(BLOCK2.toVector2d(), BLOCK2.toRotation2d(), new TranslationalVelConstraint(500)/*, accelConstraint*/)
+                .build();
+
+        Actions.runBlocking(traj2);
         goTo(BLOCK2DEPOSIT);
         goTo(BLOCK2);
         setSpeed(Speed.SLOW);
